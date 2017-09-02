239a1027fac4f7bca3da614622c177dd4689f467
Chris Barcia <cabed21@gmail.com>
2017-09-02 03:09:58 -0400

    added placeholder static players and scores, setting up dynamic score system

--- client/main.js
+++ client/main.js
@@ -2,9 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+const players = [{
+  _id: '1',
+  name: 'Christy',
+  score: 99
+}, {
+  _id: '2',
+  name: 'Chris',
+  score: -1
+}, {
+  _id: '3',
+  name: 'Leah',
+  score: 4
+}];
+
+const renderPlayers = function (playersList) {
+  return playersList.map(function (player) {
+    return <p key={player._id}>{player.name} has {player.score} point(s)</p>;
+  });
+};
 
 Meteor.startup(function () {
+  let title = 'Score Keep'
   let name = 'Chris';
-  let jsx = <p>Hello {name}!</p>;
+  let jsx = (
+    <div>
+      <h1>{title}</h1>
+      <p>Hello {name}!</p>
+      <h2>Leaderboard.</h2>
+      {renderPlayers(players)}
+  </div>
+  );
   ReactDOM.render(jsx, document.getElementById('app'));
 });

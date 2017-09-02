ba5640534514ced4fd446286deba3886e6711a51
Chris Barcia <cabed21@gmail.com>
2017-09-02 02:18:26 -0400

    added react

---
+++ imports/utils.js
@@ -0,0 +1,10 @@
+console.log('Log form /imports/utils.js');
+
+
+export let greetUser = function () {
+  return 'Hello user!';
+};
+
+export let name = 'Chris Barcia';
+
+export default 'Default val';
